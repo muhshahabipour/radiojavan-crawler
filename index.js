76385bb123d73b08976f9c212fc7ec01dadfb6e2
Muhammad Shahabi <muh.shahabi@gmail.com>
2018-08-04 15:03:28 +0430

    update

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const AddressBuilder = require('./modules/AddressBuilder')
 
 const app = express();
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
+
+
+
 
 app.get('/', async function (req, res) {
+
+  res.render("index.html", {});
+
+})
+
+app.post('/fetch', function (req, res) {
   // https://host2.rjmusicmedia.com/media/mp3/mp3-256/Sasy-Che-Pesari.mp3
   // "https://www.radiojavan.com/mp3s/mp3/Sasy-Che-Pesari"
   // var address = RjUtility.getAddress("https://www.radiojavan.com/videos/video/siamak-abbasi-man-divane-nistam");
 
-  let crawler = new AddressBuilder.Builder("https://www.radiojavan.com/mp3s/mp3/Sasy-Che-Pesari").detectType().crawler();
+  var url = req.body.url;
+
+  let crawler = new AddressBuilder.Builder(url).detectType().crawler();
   await crawler.then((response) => {
     response.getDownloadLink().then((address) => {
       res.send(address);
     })
+  }).catch(() => {
+    res.send("not found!!");
   });
-
-
 })
 
-app.get('/domain', function (req, res) {
-  res.send("domain");
-})
-
-// Set template engine
-app.set('view engine', 'ejs')
 
 // Bind the app to a specified port
 var port = process.env.PORT || 3000;
